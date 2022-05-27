9317374e593a155c5c839cab631b9f70f8e32677
Shani Bhingaradiya <shanibhingaradiya@gmail.com>
2022-05-27 09:35:52 +0530

    server connected

--- server/index.js
+++ server/index.js
@@ -2,3 +2,18 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://shanipatel12:shanipatel123@cluster0.baxyndf.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`server running on port ${5000}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
