9dbbb7f43c789c8e7b6b53b08c6465376e444f10
Shani Bhingaradiya <shanibhingaradiya@gmail.com>
2022-05-30 16:13:00 +0530

    signup/signin form done

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 
 
@@ -15,10 +17,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://shanipatel12:shanipatel123@cluster0.baxyndf.mongodb.net/?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb+srv://shanipatel12:shanipatel123@cluster0.baxyndf.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`server running on port ${5000}`)))
     .catch((error) => console.log(error.message));
 
